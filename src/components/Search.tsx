1188c4644bea72d1729a574fd07bd4fe7d553025
adrianamiclos <adriana.miclos@manifesto.co.uk>
2020-06-15 00:47:35 +0100

    Add sort by

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,10 @@
 import algoliasearch from 'algoliasearch/lite';
 import {
     InstantSearch,
-    ClearRefinements,
     Configure,
     SearchBox,
     connectRefinementList,
-    connectHits, connectHighlight, Snippet,
+    connectHits, connectHighlight, Snippet, connectStats, connectSortBy,
 } from 'react-instantsearch-dom';
 import * as React from 'react';
 import './Search.css';
@@ -13,6 +12,7 @@ import {CloseIcon, SearchIcon} from "nhsuk-react-components";
 import {Hit, HitsProvided, RefinementListProvided} from "react-instantsearch-core";
 import Checkboxes from "nhsuk-react-components/lib/components/checkboxes";
 import Pagination from "./SearchPagination";
+import Select from "nhsuk-react-components/lib/components/select";
 
 const searchClient = algoliasearch('02F3VJA4YI', 'b20c325ddd93b08a4b9fa792e619d161');
 
@@ -26,32 +26,39 @@ export function Search() {
 
             <InstantSearch searchClient={searchClient} indexName="brdocs">
                 <div className="left-panel">
-                    <ClearRefinements/>
-                    <div style={{backgroundColor: 'white', margin: '50px 20px 20px 0px', padding: '10px' }}>
-                        <h5>Category</h5>
-                        <CustomRefinementList attribute="category"/>
-                    </div>
+                    <div className="custom-refinement-section">
+                        <div className="custom-refinement-list">
+                            <h5>Category</h5>
+                            <CustomRefinementList attribute="category"/>
+                        </div>
 
-                    <div style={{backgroundColor: 'white', margin: '10px 20px 20px 0px', padding: '10px' }}>
-                    <h5>Region</h5>
-                    <CustomRefinementList attribute="region"/>
-                    </div>
+                        <div className="custom-refinement-list">
+                            <h5>Region</h5>
+                            <CustomRefinementList attribute="region"/>
+                        </div>
 
-                    <div style={{backgroundColor: 'white', margin: '10px 20px 20px 0px', padding: '10px' }}>
-                    <h5>Speciality</h5>
-                    <CustomRefinementList attribute="speciality"/>
+                        <div className="custom-refinement-list">
+                            <h5>Speciality</h5>
+                            <CustomRefinementList attribute="speciality"/>
+                        </div>
                     </div>
-
                     <Configure hitsPerPage={1}/>
                 </div>
                 <div className="right-panel">
                     <div className="custom-search">
                         <SearchBox submit={<SearchIcon/>} reset={<CloseIcon/>}/>
                     </div>
+                    <CustomSortBy
+                        defaultRefinement="brdocs"
+                        items={[
+                            {value: 'brdocs', label: 'Relevance'},
+                            {value: 'brdocs_date_desc', label: 'Most recently modified'}
+                        ]}
+                    />
+                    <CustomStats/>
                     <CustomHits/>
                     <Pagination/>
                 </div>
-
             </InstantSearch>
         </div>
     );
@@ -84,23 +91,19 @@ interface HitProps {
 
 const Hits = ({hits}: HitsProvided<Hit>) => (
     <div>
-        <p className="nhsuk-body-s nhsuk-u-margin-bottom-3">
-            Found <b>{hits.length}</b> matching results.
-        </p>
-
         <ul className="nhsuk-list nhsuk-list--border">
             {hits.map(hit => (
                 <div>
                     <li>
-                        <h2 className="nhsuk-u-margin-bottom-1" style={{"fontWeight": 400, "fontSize": "19px"}}><a
-                            className="app-search-results-item">
-                            <CustomHighlight attribute="title" hit={hit}/>
-                        </a>
-
+                        <h2 className="nhsuk-u-margin-bottom-1" style={{"fontWeight": 400, "fontSize": "19px"}}>
+                            <a
+                                className="app-search-results-item">
+                                <CustomHighlight attribute="title" hit={hit}/>
+                            </a>
                         </h2>
                         <p className="nhsuk-body-s nhsuk-u-margin-top-1">
                             <CustomHighlight attribute="introduction" hit={hit}/>
-                            <Snippet hit={hit} attribute="content" />;
+                            <Snippet hit={hit} attribute="content"/>;
                             <strong>
                                 <CustomHighlight attribute="category" hit={hit}/>
                             </strong>
@@ -119,7 +122,6 @@ const RefinementList = ({items, refine}: RefinementListProvided) => (
         {items.map(item => (
             <Checkboxes.Box
                 key={item.label}
-
                 onClick={() => {
                     return refine(item.value);
                 }}
@@ -131,3 +133,39 @@ const RefinementList = ({items, refine}: RefinementListProvided) => (
 );
 
 const CustomRefinementList = connectRefinementList(RefinementList);
+
+interface StatsProvided {
+    nbHits: number;
+    processingTimeMS: number;
+}
+
+const Stats = ({nbHits}: StatsProvided) => (
+    <p className="nhsuk-body-s nhsuk-u-margin-bottom-3" style={{paddingLeft: "65%", paddingBottom: '20px'}}>
+        Found <b>{nbHits}</b> matching results.
+    </p>
+);
+
+const CustomStats = connectStats(Stats);
+
+
+const SortBy = ({items, refine, createURL}: any) => (
+    <Select
+        id="select-1"
+        label="Sort by"
+        onChange={(event: any) => {
+            event.preventDefault();
+            refine(event.target.value)
+        }}
+        style={{fontSize: "16px"}}
+    >
+        {items.map((item: { value: string, label: string, isRefined: boolean }) => (
+            <Select.Option
+                value={item.value}
+                href={createURL(item.value)}>
+                {item.label}
+            </Select.Option>
+        ))}
+    </Select>
+);
+
+const CustomSortBy = connectSortBy(SortBy);
